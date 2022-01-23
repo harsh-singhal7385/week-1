21f722e89951b264f1ba3231243941a3a66c2e61
harsh-singhal7385 <harsh-singhal7385@github.com>
2022-01-24 03:12:49 +0530

    update 23th jan

--- assignment-3-python-flask/flask/app.py
+++ assignment-3-python-flask/flask/app.py
@@ -1,10 +1,64 @@
-from flask import Flask,render_template,request
+from datetime import datetime
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydatabase.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+class ToDoList(db.Model):
+    index = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(400), nullable = False)
+    datetime_var = db.Column(db.DateTime, default = datetime.utcnow)
 
+
+@app.route('/', methods = ['GET', 'POST'])
+def home_page():
+    
+    if request.method == "GET":
+        titleData = request.form['title']
+        descriptionData = request.form['description']
+        todo = ToDoList(title = titleData, description = descriptionData)
+        db.session.add(todo)
+        db.session.commit()
+        allTodo = ToDoList.query.all()
+    
+    return render_template('./index.html',allTodo = "")
+
+
+@app.route('/add', methods = ['GET', 'POST'])
+def addData():
+    if request.method == "POST":
+        titleData = request.form['title']
+        descriptionData = request.form['description']
+        todo = ToDoList(title = titleData, description = descriptionData)
+        db.session.add(todo)
+        db.session.commit()
+        allTodo = ToDoList.query.all()
+    
+        return redirect('/',)
+
+
+@app.route('/update', methods = ['GET', 'POST'])
+def updateData():
+    return redirect('/')
+
+
+@app.route('/delete', methods = ['GET', 'POST'])
+def deleteData():
+    return redirect('/')
+
+
+@app.route('/search', methods = ['GET', 'POST'])
+def searchData():
+    return redirect('/')
+
+
+@app.route('/getlist', methods = ['GET', 'POST'])
+def getlistData():
+    return redirect('/')
 
 
 if __name__== '__main__':
